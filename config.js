5c38f3a25087858445e68aee913b8a4bce1a463e
Youssefbotyouyou <144582568+Youssefbotyouyou@users.noreply.github.com>
2023-09-10 11:33:45 +0300

    Update config.js

--- config.js
+++ config.js
@@ -7,8 +7,8 @@ import axios from 'axios'
 
 
 global.owner = [
-  ['201206178781', 'ɢᴏᴊᴏ', true],
-  ['967780763155', 'BOT', true], 
+  ['201223089189', 'ɢᴏᴊᴏ', true],
+  ['16305199949', 'BOT', true], 
   [''] 
 ] //Number of owners
 
